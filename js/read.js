d26b27d1903d7df7a09aece088f0d088aa03405c
Ludovica Russo <72860003+LudovicaErre@users.noreply.github.com>
2023-02-17 19:30:41 +0100

    Add files via upload

---
+++ js/read.js
@@ -0,0 +1,26 @@
+function hideAway() {
+            var pText = document.getElementsByClassName('imgpar')[0];
+            if (pText.style.display == 'block') {
+                pText.style.display = 'none';
+            }else{
+                pText.style.display = 'block';
+            }
+        }
+
+function hideAway1() {
+            var pText = document.getElementsByClassName('imgpar1')[0];
+            if (pText.style.display == 'block') {
+                pText.style.display = 'none';
+            }else{
+                pText.style.display = 'block';
+            }
+        }
+        
+function hideAway2() {
+            var pText = document.getElementsByClassName('imgpar2')[0];
+            if (pText.style.display == 'block') {
+                pText.style.display = 'none';
+            }else{
+                pText.style.display = 'block';
+            }
+        }
